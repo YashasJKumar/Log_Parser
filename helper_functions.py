cd457db4e83ee9eef4ef100697808cb2a13b1066
Yashas J Kumar <96472550+YashasJKumar@users.noreply.github.com>
2024-08-07 09:41:07 +0530

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -12,13 +12,11 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from dotenv import load_dotenv
 from parser import parse_log
 from langchain_groq import ChatGroq
 
-# ------------------------------------ Loading .env file ---------------------------------------------------------------
+# ------------------------------------ Loading environment variables ---------------------------------------------------------------
 
-load_dotenv()
 groq_api_key = os.getenv('GROQ_API_KEY')
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 
